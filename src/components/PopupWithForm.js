b681a6ba48beeb3bcc6f16d02adce81ba9ebe46f
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-11-04 14:52:41 +0300

    Add files via upload

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 class PopupWithForm extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitted: false,
+  };
+
+  }
  
   handleSubmit = () => {
     this.setState({ submitted: true });
