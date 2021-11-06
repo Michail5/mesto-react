fae99dc843440b8b648105a697628571e789d911
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-11-06 13:28:46 +0300

    Add files via upload

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import API, { apiData } from '../utils/Api';
 import Card from './Card';
 
@@ -13,68 +13,61 @@ function Main(props) {
   React.useEffect(() => {
     // запрос в API за пользовательскими данными
     apiData.getUserData()
-    .then(res => {
-      handleGetUserName(res.name);
-      handleGetUserDescription(res.about); 
-      handleGetUserAvatar(res.avatar);
-    })
-    .catch((err) => {
-      console.log(err); // "Что-то пошло не так: ..."
-    })
+      .then(res => {
+        handleGetUserName(res.name);
+        handleGetUserDescription(res.about);
+        handleGetUserAvatar(res.avatar);
+      })
+      .catch((err) => {
+        console.log(err); // "Что-то пошло не так: ..."
+      })
   }, []);
 
   React.useEffect(() => {
     // запрос в API за карточками мест
     apiData.getInitialCards()
-    .then(items => {
-      setCards(items);
-    })
-    .catch((err) => {
-      console.log(err); // "Что-то пошло не так: ..."
-    })
+      .then(items => {
+        setCards(items);
+      })
+      .catch((err) => {
+        console.log(err); // "Что-то пошло не так: ..."
+      })
   }, []);
 
-  function CardList(props) {
-    const cards = props.cards;
-    
-    return (
-      <ul className="elements__list">
-      {cards.map((card) =>
-         <Card card={card} key={card._id} onCardClick={props.onCardClick} />
-       )}
-</ul> 
-    );
-  }
-    return(
-        <main className="content">
-        <section className="profile">
-          <div 
-          onClick={props.onEditAvatar} 
+  return (
+    <main className="content">
+      <section className="profile">
+        <div
+          onClick={props.onEditAvatar}
           className="profile__avatar-container"
-          >
-            <img
-              src={userAvatar}
-              alt="Жак-Ив Кусто"
-              className="profile__avatar"
-            />
-          </div>
-          <div className="profile__info">
-            <h1 className="profile__name">{userName}</h1>
-            <p className="profile__description">{userDescription}</p>
-            <button
-            onClick={props.onEditProfile}
-            type="button" 
-            className="profile__edit-button" 
-            />
-          </div>
-          <button
-          onClick={props.onAddPlace}
-          type="button" 
-          className="profile__add-button" 
+        >
+          <img
+            src={userAvatar}
+            alt="Жак-Ив Кусто"
+            className="profile__avatar"
           />
-        </section>
-        <CardList cards={cards} onCardClick={props.onCardClick}/>
-      </main>
-    )
+        </div>
+        <div className="profile__info">
+          <h1 className="profile__name">{userName}</h1>
+          <p className="profile__description">{userDescription}</p>
+          <button
+            onClick={props.onEditProfile}
+            type="button"
+            className="profile__edit-button"
+          />
+        </div>
+        <button
+          onClick={props.onAddPlace}
+          type="button"
+          className="profile__add-button"
+        />
+      </section>
+  <ul className="elements__list">
+        {cards.map((card) => (
+          <Card card={card} key={card._id} onCardClick={props.onCardClick} />
+        ))}
+      </ul>
+    </main>
+  )
 }
 export default Main;
