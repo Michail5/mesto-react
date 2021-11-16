2bf00adfe23d14ef3870c96306c1ef3b9b229634
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-11-16 22:05:50 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/components/App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import PopupWithForm from './components/PopupWithForm';
-
-
 
 ReactDOM.render(
   <React.StrictMode>
