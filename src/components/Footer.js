da9a7c9c690f1a31a48e984722286b79ceb56703
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-11-16 22:19:15 +0300

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__text">&copy; 2021  Michael Yakovlev</p>
-    </footer>
-  );
-}
-
-export default Footer;
