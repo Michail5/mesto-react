d71a57c18502be91740910eec2da5778da60b635
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-11-19 22:38:12 +0300

    Add files via upload

---
+++ src/components/AddPlacePopup.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import PopupWithForm from './PopupWithForm';
+
+function EditAvatarPopup({ isOpen, onClose, onAddPlace }) {
+  const [name, setName] = useState('');
+  const [link, setLink] = useState('');
+  const [buttonTitle, setButtonTitle] = useState('Создать');
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleLinkChange = (e) => setLink(e.target.value);
+  const resetInput = () => {
+    setName('');
+    setLink('');
+    setButtonTitle('Создать');
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setButtonTitle('Обработка...');
+    onAddPlace({ name, link })
+      .then(() => resetInput())
+      .catch(() => setButtonTitle('Ошибка!'));
+  };
+
+  return (
+    <PopupWithForm
+      name="placeForm"
+      title="Новое место"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+      btnCaption={buttonTitle}
+    >
+      <input
+        id="inputPlaceName"
+        className="popup__input"
+        name="name"
+        type="text"
+        value={name || ''}
+        onChange={handleNameChange}
+        title="назовите место"
+        placeholder="Название"
+        minLength="2"
+        maxLength="30"
+        required
+      />
+      <span id="inputPlaceName-error" className="popup__error" />
+      <input
+        id="inputPlaceLink"
+        className="popup__input"
+        name="link"
+        type="url"
+        value={link || ''}
+        onChange={handleLinkChange}
+        title="укажите путь"
+        placeholder="Ссылка на картинку"
+        required
+      />
+      <span id="inputPlaceLink-error" className="popup__error" />
+    </PopupWithForm>
+  );
+}
+
+export default EditAvatarPopup;
